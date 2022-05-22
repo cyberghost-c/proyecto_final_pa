3443880b5aa49db361d235cab859442c4fa6591f
Sergio <sergio.florez01@correo.usa.edu.co>
2022-05-22 01:11:37 -0500

    mensaje nuevo en master

--- proyecto_final.cpp
+++ proyecto_final.cpp
@@ -7,5 +7,6 @@ int main()
     cout<<"hola mundo"<<endl;
     cout<<"como andan"<<endl;
     cout<<"estos son los cambios de una rama";
+    cout<<"ahora tambien hice cambios en el master";
     return 0;
 }
